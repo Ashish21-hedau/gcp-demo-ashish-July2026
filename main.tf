ae56f91582416d8650bf177f5a19f0511d19602e
Ashish Kumar Hedau <ashishkhedau182@gmail.com>
2026-07-14 00:35:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "no-public-access" {
-  name          = "ashish-hedaul-terraform-2026-12345""
+  name          = "ashish-hedaul-terraform-2026-12345"
   location      = "US"
   force_destroy = true
   public_access_prevention = "enforced"
